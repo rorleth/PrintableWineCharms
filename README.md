ed3862b7b70fed258debe490dc3781f3c1eea77e
rorleth <rorleth@hotmail.com>
2024-03-27 21:22:42 -0700

    Deploy website - based on 537e1abb1d86380cc0feaaae8118b3318ed3af87

--- README.md
+++
@@ -1,4 +0,0 @@
-# PrintableWineCharms
-
-Can be used to generate a printable 3D model of a personalized wine charm (a thingy to hang on to the stem of a wine glass to help figure out whose glass that one is).
-Text, font and sizes are customizable. Use OpenSCAD to open.
---
+++ assets/js/3a6c06cd.06f30613.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[8429],{990:s=>{s.exports=JSON.parse('{"label":"docusaurus","permalink":"/PrintableWineCharms/blog/tags/docusaurus","allTagsPath":"/PrintableWineCharms/blog/tags","count":4,"unlisted":false}')}}]);
